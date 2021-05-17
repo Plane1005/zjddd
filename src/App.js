ed7374896fa3a8ba4ad49a007bce6e543416b592
Jet1005 <1352351506@qq.com>
2021-05-17 21:40:27 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import NavWrapper from './component/NavWrapper/index.js'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app-root'>
+        <NavWrapper/>
+      </div>
+    )
+  }
+}
